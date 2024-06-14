feb4b63d7572208e22ec0bc7ead1f6c8ecc85e6e
Hy <hyhuynh222@gmail.com>
2024-06-14 08:39:31 -0700

    Added invalid input logger

--- src/main/java/genspark/pj/CustomerManagementSystem/Aspect/LoggingAspect.java
+++ src/main/java/genspark/pj/CustomerManagementSystem/Aspect/LoggingAspect.java
@@ -1,15 +1,22 @@
 package genspark.pj.CustomerManagementSystem.Aspect;
 
+import genspark.pj.CustomerManagementSystem.Entity.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Aspect
 @Component
@@ -31,14 +38,32 @@ public class LoggingAspect {
         Object[] args = joinPoint.getArgs();
         String argsString = Arrays.toString(args); // Or Arrays.stream(args).map(Object::toString).collect(Collectors.joining(", "));
         log.info("Method executed: " + methodName + ", Arguments: " + argsString +", Return: " + result);
-//        if (result.equals()){
-//            log.info("No Customer with the" + joinPoint.getSignature().getDeclaringTypeName());
-//        }
+        if (isEmpty(result)){
+            log.info("No Customer with the criteria: " + argsString);
+        }
     }
 
-//    private boolean isEmpty(Object obj){
-//        if (obj = null)
-//    }
+    private boolean isEmpty(Object obj) {
+        if (obj == null) {
+            return true;
+        }
+        if (obj instanceof String) {
+            return ((String) obj).trim().isEmpty();
+        }
+        if (obj instanceof Collection) {
+            return ((Collection<?>) obj).isEmpty();
+        }
+        if (obj instanceof Map) {
+            return ((Map<?, ?>) obj).isEmpty();
+        }
+        if (obj.getClass().isArray()) {
+            return Arrays.asList((Object[]) obj).isEmpty();
+        }
+        if (obj instanceof Customer){
+            return  ((Customer) obj).getName().isEmpty();
+        }
+        return false;
+    }
 
 
 }
